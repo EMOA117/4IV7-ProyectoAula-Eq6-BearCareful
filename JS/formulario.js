99adc9c89d78b6d194d45cb02153f85484cc13ee
Edgar Montes de Oca Arellano <79484590+EMOA117@users.noreply.github.com>
2021-03-27 14:47:47 -0600

    corrección de validación

--- JS/formulario.js
+++ JS/formulario.js
@@ -1,45 +1,84 @@
-
-function validar(formulario) {
+function validar(formulario){
     //obtener los valores del formulario
-    if (formulario.nombre.value.length < 5) {
+   
+    if(formulario.nombre.value.length < 5){
         alert("Escribe mas de 5 caracteres en el campo nombre");
         formulario.nombre.focus();
-        return false;
+    return false;
+    }
+
+    if(formulario.email.value.length == 0){
+        alert("Escribe un email");
+        formulario.email.focus();
+    return false;
+
+    }
+    var txt = formulario.email.value;
+    //patron
+    var b = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+
+    var esValido = b.test(txt);
+
+    if(esValido==false){
+        alert("El correo electrónico no es valido");
+        formulario.email.focus();
+    return false;
+    }
+
+    if(formulario.telefono.value.length < 10){
+        alert("Escribe al menos 10 digitos en Número de contacto");
+        formulario.telefono.focus();
+    return false;
+    }
+
+    if(formulario.telefono.value.length > 10){
+        alert("Escribe menos  de 10 digitos en Número de contacto");
+        formulario.telefono.focus();
+    return false;
     }
 
 
-    var checkOK = "QWERTYUIOPASDFGHJKLÑZXCVBNM"
-        + "qwertyuiopasdfghjklñzxcvbnm";
+    if(formulario.mensaje.value.length == 0){
+        alert("Escribe un mensaje");
+        formulario.mensaje.focus();
+    return false;
+
+    }
+
+
+    /*
+    vamos a crear una opcion para que solo se pueda ingresar
+    letras dentro del campo nombre
+    */
+
+    var checkOK = " QWERTYUIOPASDFGHJKLÑZXCVBNM"
+    + " qwertyuiopasdfghjklñzxcvbnm";
 
     var checkStr = formulario.nombre.value;
 
     var allvalid = true;
 
-    for (var i = 0; i < checkStr.length; i++) {
+    for(var i = 0; i < checkStr.length; i++){
         var ch = checkStr.charAt(i);
-        for (var j = 0; j < checkOK.length; j++)
-            if (ch == checkOK.charAt(j))
-                break;
-        if (j == checkOK.length) {
+        for(var j = 0; j < checkOK.length; j++)
+        if(ch == checkOK.charAt(j))
+            break;
+        if(j == checkOK.length){
             allvalid = false;
             break;
         }
     }
 
-    if (!allvalid) {
+    if(!allvalid){
         alert("Escribe solo letras en el campo nombre");
         formulario.nombre.focus();
         return false;
     }
-    if (formulario.mensaje.value.length < 5) {
-        alert("Escribe mas de 5 caracteres en el campo nombre");
-        formulario.nombre.focus();
-        return false;
-    }
 
-
-    var checkOK = "QWERTYUIOPASDFGHJKLÑZXCVBNM"
-        + "qwertyuiopasdfghjklñzxcvbnm";
+    var checkOK = " QWERTYUIOPASDFGHJKLÑZXCVBNM"
+        + " qwertyuiopasdfghjklñzxcvbnm"+
+        "!?¡¿$%#&"
+        +"1234567890";
 
     var checkStr = formulario.mensaje.value;
 
@@ -62,35 +101,34 @@ function validar(formulario) {
         return false;
     }
 
+
+    /*
+    vamos a validar la entrada de numeros
+    */ 
+
     var checkOK = "1234567890";
 
     var checkStr = formulario.telefono.value;
 
     var allvalid = true;
 
-    for (var i = 0; i < checkStr.length ; i++) {
+    for(var i = 0; i < checkStr.length; i++){
         var ch = checkStr.charAt(i);
-        for (var j = 0; j < checkOK.length; j++)
-            if (ch == checkOK.charAt(j))
-                break;
-        if (j == checkOK.length) {
+        for(var j = 0; j < checkOK.length; j++)
+        if(ch == checkOK.charAt(j))
+            break;
+        if(j == checkOK.length){
             allvalid = false;
             break;
         }
     }
 
-    if (!allvalid) {
+    if(!allvalid){
         alert("Escribe solo numeros en el campo Telefono");
         formulario.telefono.focus();
         return false;
     }
 
-    var txt = formulario.email.value;
 
-    
-    var b = /^[^@\s]+@[^@\.\s]+(\.[^@\.\s]+)+$/;
 
-    alert("Email " + (b.test(txt) ? "" : " no ") + " valido");
-
-    return b.test(txt);
 }
